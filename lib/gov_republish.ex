c79049d37085f261bba31ab1cdd90511145f80ca
John B. Casey <caseyjohnb@gmail.com>
2025-02-11 21:42:48 -0500

    initial commit

---
+++ lib/gov_republish.ex
@@ -0,0 +1,18 @@
+defmodule GovRepublish do
+  @moduledoc """
+  Documentation for `GovRepublish`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GovRepublish.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gov_republish_test.exs
@@ -0,0 +1,8 @@
+defmodule GovRepublishTest do
+  use ExUnit.Case
+  doctest GovRepublish
+
+  test "greets the world" do
+    assert GovRepublish.hello() == :world
+  end
+end
